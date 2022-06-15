bde5b26fa15b04e99e09e0646c96192d7556a744
Lucas <lucastecher97421fabien@gmail.com>
2022-06-15 19:20:40 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const rad1 = document.getElementById("radio1");
+const rad2 = document.getElementById("radio2");
+
+document.querySelector(".txt1").classList.add("active");
+
+let typeContent;
+
+rad1.addEventListener("click", () => {
+    document.querySelectorAll(".active").forEach(el => {
+        el.classList.remove("active");
+    })
+    document.querySelector(".txt1").classList.add("active");
+    typeContent = true;
+})
+
+rad2.addEventListener("click", () => {
+    document.querySelectorAll(".active").forEach(el => {
+        el.classList.remove("active");
+    })
+    document.querySelector(".txt2").classList.add("active");
+    typeContent = false;
+})
+
+document.querySelector("#pull").addEventListener("click", () => {
+    if (typeContent) {
+        alert("nsfw content");
+    } else if (!typeContent) {
+        alert("sfw content");
+    }
+})
